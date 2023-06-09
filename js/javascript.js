7815e311bac8ed1bf43a3806e99195d790786342
eliseuramos93 <eliseu.mec@gmail.com>
2023-06-08 23:10:26 -0300

    Add complete UI, pivot to PKMN game

--- js/javascript.js
+++ js/javascript.js
@@ -1,12 +1,4 @@
-/*==============================================================================
-choose rock, paper or scissors for computer => getComputerChoice();
-  [OK] make computer choose a random number from 0 to 2 
-  => getComputerRandomNumber();
-  [OK] create rule to correlate each value to an option (rock, paper or 
-  scissors) => 0 is Paper, 1 is Rock, 2 is Scissors (alphabetical order)
-  [OK] assign the number to an option => if inside getComputerChoice();
-  [OK] store the computer's choice in a variable => let computerSelection*/
-//==============================================================================
+                        //=== GAME LOGIC ===//
 
 function generateRandomNumber() {
   return Math.floor(Math.random() * 3);
@@ -14,82 +6,30 @@ function generateRandomNumber() {
 
 function getComputerChoice(randomInt) {
   if (randomInt === 0) {
-    return "paper";
+    return "Charmander";
   } else if (randomInt === 1) {
-    return "rock";
+    return "Squirtle";
   } else if (randomInt === 2) {
-    return "scissors"
+    return "Bulbasaur"
   } else {
     return "Error - Problem in the RNG"
   };
 }
-
-/*==============================================================================
-get choice from player => getPlayerChoice();
-  [OK] ask the user if he wants rock, paper or scissors
-  [OK] transform input to lowercase to avoid case-sensitive issues;
-  [OK] remove trailing and following spaces w/ trim
-  [OK] check if player made a correct input (rock, paper or scissors) 
-  => let validChoice;
-    [OK] if not, keep asking until they get it right => while loop, 
-    sentry invalidInput
-  [OK] store the players choice in a variable => let playerSelection;*/
-//==============================================================================
-
-function getPlayerChoice() {
-  let invalidInput = true;
-  let playerInput;
-
-  while (invalidInput === true) {
-    playerInput = prompt("Make a choice: Rock, paper or scissors?");
-    playerInput = playerInput.toLowerCase();
-    playerInput = playerInput.trim();
-  
-    if (playerInput === "rock" || playerInput === "paper" || 
-        playerInput === "scissors") {
-
-      invalidInput = false;
-    } else {
-      alert("Invalid option. Choose only between rock, paper and scissors.");
-    }
-  }
-
-  return playerInput;
-}
-
-/*============================================================================== 
-compare player and computer's choices [playRound()]
-  [OK]if equal, it's a tie
-    [OK] if tied, store NULL in a variable;
-  [OK]else if different, apply a rule to determine the winner;
-    [OK]if player won, store TRUE in a variable
-    [OK]if player lost, store FALSE in a variable*/
-//==============================================================================
 
 function playRound(playerSelection, computerSelection) {
   if (computerSelection === playerSelection) {
     return null;
 
   } else if (
-    (computerSelection === "paper" && playerSelection === "scissors") ||
-    (computerSelection === "rock" && playerSelection === "paper") ||
-    (computerSelection === "scissors" && playerSelection === "rock")) {  
+    (computerSelection === "Charmander" && playerSelection === "Squirtle") ||
+    (computerSelection === "Squirtle" && playerSelection === "Bulbasaur") ||
+    (computerSelection === "Bulbasaur" && playerSelection === "Charmander")) {  
       return true;
 
   } else {
     return false;
   };
 }
-// =============================================================================
-/* create a function game() to play a 5-round game that keeps score and reports 
-a winner at the end*/
-  // [OK] create a variable playerScore that starts at 0;
-  // [OK] create a variable computerScore that starts at 0;
-  // [OK] play a round of the game [playRound];
-  // [OK] update the score after the round [updateScore];
-  // [OK] display the results and the updated score to the player [showScore];
-  // [OK] repeat the game until either the player or computer reaches 3 points;
-// =============================================================================
 
 function updateScore(roundResults, score) {
   if (roundResults === true) {
@@ -112,71 +52,129 @@ function showScore(
   roundResults) {
 
   if(roundResults === true) {
-    alert(`Yes, ${playerSelection} beats ${computerSelection}, you WON! 
-    The score now is: You ${playerScore} x ${computerScore} CPU`);
+    resultsBox.innerHTML = 
+    `${playerSelection} is super effective v. ${computerSelection}! 
+    The score now is: 
+    <br><br>You ${playerScore} x ${computerScore} RIVAL`;
 
   } else if (roundResults === false) {
-    alert(`Oh no, ${playerSelection} loses to ${computerSelection}, you LOST! 
-    The score now is: You ${playerScore} x ${computerScore} CPU`);
+    resultsBox.innerHTML = `${playerSelection} is not every effective v. ${computerSelection}... 
+    The score now is: 
+    <br><br>You ${playerScore} x ${computerScore} RIVAL`;
 
   } else if (roundResults === null) {
-    alert(`You both chose ${playerSelection}, it's a tie! 
-    The score now is: You ${playerScore} x ${computerScore} CPU`);
+    resultsBox.innerHTML = 
+    `You both chose ${playerSelection}, it's a tie!
+    The score now is: 
+    <br><br>You ${playerScore} x ${computerScore} RIVAL`;
 
   } else {
-    alert('Game error: debug the code.');
+    resultsBox.textContent = 'Game error: debug the code.';
   };
 }
 
+let playerScore = 0;
+let computerScore = 0;
+
 function game(playerButton) {
-  let playerScore = 0;
-  let computerScore = 0;
+
   let scoreArray = [playerScore, computerScore];
-  let keepGoing = false;
+  let keepGoing = true;
   let computerSelection;
   let playerSelection = playerButton;
   let roundResults;
 
-   do {
     computerSelection = getComputerChoice(generateRandomNumber());
     roundResults = playRound(playerSelection, computerSelection);
     score = updateScore(roundResults, scoreArray);
     playerScore = scoreArray[0];
     computerScore = scoreArray[1];
 
-    if (playerScore > 2 || computerScore > 2) {
-      keepGoing = false;
-    }
     showScore(
       playerSelection, 
       computerSelection, 
       playerScore, 
       computerScore, 
       roundResults);
-  } while (keepGoing)
-
-  if (playerScore > computerScore) {
-    alert('YOU WON THE GAME!');
-  } else {
-    alert ('You lost the game...');
+   
+  if (playerScore > 4) {
+    resultsBox.textContent = 'PKMN TRAINER RIVAL was defeated!';
+  } else if (computerScore > 4) {
+    resultsBox.textContent = "You're out of usable POKÉMON!";
   }
 }
 
-const container = document.querySelector('#test');
-console.log(container);
+                      //=== USER INTERFACE ===//
+const header = document.querySelector('#header');
+const container = document.querySelector('#pokemon-selection');
+const resultsBox = document.querySelector('#results-box');
+header.classList.add('text-box');
+container.classList.add('choose-pokemon');
+resultsBox.classList.add('text-box');
 
-const rockButton = document.createElement('button');
-const paperButton = document.createElement('button');
-const scissorsButton = document.createElement('button');
+// CTA text display;
+function showOakMessage() {
+  header.textContent = "OAK: Choose your starter!";  
+}
+showOakMessage();
 
-rockButton.textContent = "Play with Rock!";
-paperButton.textContent = "Play with Paper";
-scissorsButton.textContent = "Play with Scissors!"
+// show pokedex info on mouseover;
+function showPokedexInfo(pokemon) {
+  if (pokemon === "bulbasaur") {
+    header.textContent = "BULBASAUR: A strange seed was planted on its back at birth. The plant sprouts and grows with this Pokémon.";
+  } else if (pokemon === "charmander") {
+    header.textContent = "CHARMANDER: Obviously prefers hot places. When it rains, steam is said to spout from the tip of its tail.";
+  } else if (pokemon === "squirtle") {
+    header.textContent = "SQUIRTLE: After birth, its back swells and hardens into a shell. Powerfully sprays foam from its mouth.";
+  };
+}
 
-container.appendChild(rockButton);
-container.appendChild(paperButton);
-container.appendChild(scissorsButton);
+// create pokemon buttons
+const grassButton = document.createElement('div');
+grassButton.classList.add('bulbasaur', 'pokemon');
+const fireButton = document.createElement('div');
+fireButton.classList.add('charmander', 'pokemon');
+const waterButton = document.createElement('div');
+waterButton.classList.add('squirtle', 'pokemon');
 
-rockButton.addEventListener('click', () => game("rock"));
-paperButton.addEventListener('click', () => game("paper"));
-scissorsButton.addEventListener('click', () => game('scissors'));
+grassButton.innerHTML = "<img src=\"./img/bulbasaur.png\" height=\"300px\">";
+fireButton.innerHTML = "<img src=\"./img/charmander.png\" height=\"300px\">";
+waterButton.innerHTML = "<img src=\"./img/squirtle.png\" height=\"300px\">";
+
+container.appendChild(grassButton);
+container.appendChild(fireButton);
+container.appendChild(waterButton);
+
+
+// animated CTA text box
+const pokemons = document.querySelectorAll('.pokemon');
+pokemons.forEach(pokemon => pokemon.addEventListener(
+  "mouseover", () => {
+    const pokemonName = pokemon.classList[0];
+    showPokedexInfo(pokemonName);}
+));
+
+// return cta text on mouseout
+pokemons.forEach(pokemon => pokemon.addEventListener('mouseout', showOakMessage));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// run game on click
+grassButton.addEventListener('click', () => game("Bulbasaur"));
+fireButton.addEventListener('click', () => game("Charmander"));
+waterButton.addEventListener('click', () => game("Squirtle"));
